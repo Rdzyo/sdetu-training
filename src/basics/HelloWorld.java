0a0ad0cd2cf07a9a40807f28bf3e28bf74c6b650
Radosław <Radexy@gmail.com>
2017-12-29 23:10:48 +0100

    Modified HelloWorld app

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld
 	public static void main(String[] args)
 	{
 		System.out.println("Hello World!");
+		System.out.println("Adding code to the project");
 		System.out.print("Adding code to the project");
 	}
 }
